4c3d94811bb904008416e98758c6c55cc8f47de1
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-03-24 10:49:59 +0530

    Add files via upload

---
+++ greenCloset/controllers/includes/header.php
@@ -0,0 +1,41 @@
+<?php if (basename($_SERVER['PHP_SELF'], '.php') == "index" || basename($_SERVER['PHP_SELF'], '.php') == "product-categories"|| basename($_SERVER['PHP_SELF'], '.php') == "products"){
+    $img_url="images/logo.png";
+}
+else
+$img_url="../images/logo.png";
+
+?>
+
+<header aria-label="Main header" class="main-header__content">
+    <nav class="navbar fixed-top navbar-expand-lg" style="background-color:transparent;">
+        <div class="container-fluid">
+            <div class="brand">
+                <a class="navbar-brand" href="../index.php"><img src="<?php echo $img_url; ?>" alt="Logo"><span class="Bname" style="color:#5dd25e;text-align:justify;"><strong>greenCloset</strong></span>
+                </a>
+               
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <div class="navitems">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                       <?php if ( basename($_SERVER['PHP_SELF'], '.php') != "product-categories"){?>
+  <a class="nav-link" href="product-categories.php">Store</a><?php }else{?>
+    <a class="nav-link" href="index.php">Home</a> <?php
+  }?>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#our_initiative">Our Initiative</a>
+                        </li>
+                        <!-- <li>
+                            <a class="nav-link" href="#aboutus">About us</a>
+                        </li> -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ greenCloset/products.php
@@ -0,0 +1,258 @@
+<?php if(isset($_GET['cat'])){
+    $cat = $_GET['cat'];
+    if ($cat != "") {
+        $conn=mysqli_connect("localhost","root","","greencloset")or die(mysqli_error($conn));
+        $query = mysqli_query($conn, "SELECT * FROM products WHERE category=$cat");
+    }
+
+}
+?>
+    
+
+
+
+<html>
+    <head>    
+
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Raleway|Roboto&display=swap');
+
+body{
+  background-color: #1a1a1a !important;
+}
+
+.container{
+  margin-top: 50px;
+}
+
+h1, .card-title, a, #toast{
+  font-family: 'Roboto', sans-serif;
+}
+
+h1{
+  color: #5dd25e !important;
+}
+
+.card{
+  /* box-shadow: 0px 15px 35px -5px rgba(50, 88, 130, 0.32); */
+  border-radius: 15px;
+  transition: transform .2s;
+
+}
+
+.card-body{
+  padding: 10px;
+  margin-top: -50px;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  transition: all 1s ease-in-out;
+}
+
+.heart{
+  color: #989898;
+  margin-top: 15px;
+  margin-left: 230px;
+  font-size: 30px;
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  border-radius: 30px;
+  padding: 0px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.heart-active{
+  color: #C50707;
+}
+
+.heart:hover{
+  color: red;
+  background-color: #f9f9f9;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  transition: all 1s ease-in-out;
+}
+
+
+
+.card:hover > .card-body{
+  margin-top: -0px;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  transition: all 1s ease;
+}
+
+.card:hover{
+  transform: scale(1.02);
+}
+
+
+.first-image{
+  width: 100%;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+}
+
+.cart, .wish{
+  padding: 5px;
+  width: 50px;
+  height: 50px;
+  margin-left: -24px;
+  margin-top: 0px;
+  margin-right: 3px;
+  background-color: #fff;
+  color: #5C7CFA !important;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  text-align: center;
+  -ms-display: flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.wish{
+  color: #C50707;
+}
+
+#toast, #toast-cart{
+  padding: 10px;
+  padding-left: 5px;
+  position: fixed;
+  width: 230px;
+  height: 50px;
+  top: 0;
+  left: 80%;
+  transform:translate(-50%);
+  background-color: #EFF2FF !important;
+  color: #76777E !important;
+  padding: 1px;
+  border-radius: 8px;
+  text-align:center;
+  z-index: 1;
+  box-shadow: 0 0 20px rgba(0,0,0,0.3);
+  visibility: hidden;
+  opacity: 0;
+  -ms-display: flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+}
+#toast.show, #toast-cart.show{
+  visibility:visible;
+  animation:fadeInOut 3s;
+}
+
+@keyframes fadeInOut{
+  5%,95%{opacity:1;top:50px}
+  15%,85%{opacity:1;top:30px}
+}
+
+a{
+  z-index: -1;
+}
+            </style>
+                    <link rel="stylesheet" href="css/header.css">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    </head>
+    <body>
+    <?php require "controllers/includes/header.php" ?>
+    <div id="toast"></div>
+        <div id="toast-cart"></div>
+        <div class="container">
+        <h1 align="center">What are you looking for?</h1>
+        <hr>
+        <br>
+        <div class="row">
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;" onclick="redirectTo('pants')">
+                <img onclick="redirectTo('pants')" src="./images/1.jpeg" class="first-image" style="height: 428.74;">
+                <div class="card-body">
+                    <center>
+                        <a href="https://www.everlane.com/products/mens-authentic-straight-jean-washed-black?collection=mens-jeans" target="_blank"><h5 class="card-title">The Authentic Straight Jean</h5></a>
+                    </center>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;"  onclick="redirectTo('tshirt')">
+            <span id="heart-second" value="So" class="heart"><i onclick="wishList()" class="fas fa-heart"></i></span>
+        <img src="./images/2.jpeg" class="first-image">
+        <div class="card-body">
+            <center>
+            <a href="https://www.everlane.com/products/mens-slim-fit-performance-jean-black?collection=mens-jeans" target="_blank"><h5 class="card-title">The Slim 4-Way Stretch Organic Jean | Uniform</h5></a>
+            </center>
+        </div>
+</div>
+</div>
+    <div class="col-lg-4">
+    <div class="card" style="width: 18rem;"  onclick="redirectTo('shoes')">
+      <span id="heart-third" class="heart"><i onclick="wishList()" class="fas fa-heart"></i></span>
+  <img src="./images/3.jpeg" class="first-image">
+  <div class="card-body">
+    <center>
+    <a href="https://www.everlane.com/products/mens-slim-fit-performance-jean-black?collection=mens-jeans" target="_blank"><h5 class="card-title">The Performance Chino | Uniform</h5></a>
+    </center>
+  </div>
+</div>
+</div>
+  </div>
+  <div class="row">
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;" onclick="redirectTo('pants')">
+                <img onclick="redirectTo('pants')" src="./images/4.jpeg" class="first-image">
+                <div class="card-body">
+                    <center>
+                    <a href="https://www.everlane.com/products/mens-slim-fit-performance-jean-black?collection=mens-jeans" target="_blank"><h5 class="card-title">The Gardener Jean</h5></a>
+                    </center>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card" style="width: 18rem;"  onclick="redirectTo('tshirt')">
+            <span id="heart-second" value="So" class="heart"><i onclick="wishList()" class="fas fa-heart"></i></span>
+        <img src="./images/5.jpeg" class="first-image">
+        <div class="card-body">
+            <center>
+            <a href="https://www.everlane.com/products/mens-slim-fit-performance-jean-black?collection=mens-jeans" target="_blank"><h5 class="card-title">The Carpenter Jean</h5></a>
+            </center>
+        </div>
+</div>
+</div>
+    <div class="col-lg-4">
+    <div class="card" style="width: 18rem;"  onclick="redirectTo('shoes')">
+      <span id="heart-third" class="heart"><i onclick="wishList()" class="fas fa-heart"></i></span>
+  <img src="./images/6.jpeg" class="first-image">
+  <div class="card-body">
+    <center>
+      <a href="https://www.everlane.com/products/womens-ribbed-knit-skirt-cocoa-brown?collection=womens-bottoms" target="_blank"><h5 class="card-title">The Ribbed Knit Skirt</h5></a>
+    </center>
+  </div>
+</div>
+</div>
+  </div>
+</div>
+<script>
+    // function redirectTo(cat){
+    //     console.log("clicked");
+    //     var xmlhttp = new XMLHttpRequest();
+    //     xmlhttp.onreadystatechange = function () {
+    //         if (this.readyState == 4 && this.status == 200) {
+    //             var myObj = JSON.parse(this.responseText);
+    //             console.log(myObj[0]);
+    //             document.getElementsByTagName("body").innerHTML = myObj[0];
+                
+    //         }
+    //     };
+    //     xmlhttp.open("GET", "products.php?cat=" + cat, true);
+    //     xmlhttp.send();
+    // }
+    </script>
+    </body>
+</html>
